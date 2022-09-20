da5b3d0add03fcaa0c82d2115b64b593e88a75f7
OlenaBielova <belova.elena11@gmail.com>
2022-09-20 11:52:49 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,50 @@
 
+import throttle from "lodash.throttle";
+import Player from "@vimeo/player";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const doTimeUpdate = function (e) {
+    // console.log("timeupdate", e);
+
+    const currentTime = JSON.stringify(e);
+    
+    localStorage.setItem("videoplayer-current-time", currentTime);
+
+    console.log(localStorage);
+};
+
+player.on('timeupdate', throttle(doTimeUpdate, 1000));
+
+
+window.addEventListener('load', onPageReload);
+
+const onPageReload = function (e) {
+    // console.log("page reload");
+
+    const timeFromStorage = localStorage.getItem("videoplayer-current-time");
+    console.log(timeFromStorage);
+
+    if (timeFromStorage) {
+        const parsedTimeFromStorage = JSON.parse(timeFromStorage).seconds;
+
+        player.setCurrentTime(parsedTimeFromStorage).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+        }).catch(function(error) {
+        switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
+};
+};
+
+
+
+
