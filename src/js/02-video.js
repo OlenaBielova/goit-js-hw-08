ecf90bfc351908356530c68034379f5b3fe46f67
OlenaBielova <belova.elena11@gmail.com>
2022-09-22 21:20:26 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,44 +7,24 @@ const player = new Player(iframe);
 
 
 const doTimeUpdate = function (e) {
-    // console.log("timeupdate", e);
 
-    const currentTime = JSON.stringify(e);
+    const currentTime = e.seconds;
+    console.log(currentTime);
     
     localStorage.setItem("videoplayer-current-time", currentTime);
     
-    // console.log(localStorage);
 };
 
 player.on('timeupdate', throttle(doTimeUpdate, 1000));
 
-
 window.addEventListener('load', onPageReload);
 
 function onPageReload (e) {
-    // console.log("page reload");
 
-    const timeFromStorage = localStorage.getItem("videoplayer-current-time");
-    console.log(timeFromStorage);
+    const currentTime = localStorage.getItem("videoplayer-current-time");
+    console.log(currentTime);
 
-    if (timeFromStorage) {
-        const parsedTimeFromStorage = JSON.parse(timeFromStorage).seconds;
-
-        player.setCurrentTime(parsedTimeFromStorage).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-        }).catch(function(error) {
-        switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-        default:
-            // some other error occurred
-            break;
+    if (currentTime) {
+        player.setCurrentTime(currentTime);
     }
-});
 };
-};
-
-
-
-
